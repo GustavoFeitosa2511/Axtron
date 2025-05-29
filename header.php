eec163bd6d9f34123278cbc59c840e07516e3511
GustavoFeitosa2511 <gustavobruxo6@gmail.com>
2025-05-29 18:02:02 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Axtron - Produtos</title>
+  <link rel="website icon" type="png" href="imagens/Axtron.jfif"/>
+  <link rel="stylesheet" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+  <header class="navbar">
+    <div class="logo">Axtron</div>
+    <nav>
+      <a href="index.php">Início</a>
+      <a href="login.php">Login</a>
+      <a href="carrinho.php">Carrinho</a>
+    </nav>
+  </header
