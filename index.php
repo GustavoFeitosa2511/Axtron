d6b77aec6a8ecea7a10a6a8c2ef6ce86cd273e01
GustavoFeitosa2511 <gustavobruxo6@gmail.com>
2025-05-29 18:02:33 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include('includes/config.php');
+if (!isset($_SESSION['carrinho'])) {
+    $_SESSION['carrinho'] = [];
+}
+if (isset($_GET['add'])) {
+    $id = (int)$_GET['add'];
+    if (!in_array($id, $_SESSION['carrinho'])) {
+        $_SESSION['carrinho'][] = $id;
+    }
+}
+$result = $conn->query("SELECT * FROM produtos");
+?>
+<?php include('header.php')?>
+        <main>
+            <h1>Produtos</h1>
+            <ul>
+                <?php while ($row = $result->fetch_assoc()): ?>
+                    <li>
+                        <?php echo $row['nome']; ?> - R$ <?php echo $row['preco']; ?>
+                        <?php echo "<img src="; echo $row['imagem']; echo " />";?>
+                        <a href="index.php?add=<?php echo $row['id']; ?>">Comprar</a>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+            
+        </main>
+        
+<?php include('footer.php')?>
